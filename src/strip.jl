e1ca0f715c07511b4cb21ce8f045f586a279db1a
Evgeny Metelkin <metelkin@insysbio.com>
2021-04-06 17:34:28 +0300

    transform to Led specific bits

--- src/strip.jl
+++ src/strip.jl
@@ -12,7 +12,7 @@ end
 
 # required rate calculated based on 3 bits per signal: 1.25 us => 0.8 MHz signal => 2.4 MHz
 
-function Strip(spi::Int; freq::Int = 800)
+function Strip(spi::Int; freq::Int = 800_000)
     # init
     if spi == 1
         path = "/dev/spidev0.0"
@@ -30,13 +30,33 @@ end
 
 function send(strip::Strip, message::AbstractArray{UInt8})
     msg_transformed = _transform(message)
-    @info "Sending..."
-    spi_transfer(1, msg_transformed)
+    spi_transfer(strip.spi, msg_transformed)
 end
 
-# transforms color bits to spi specific bits
+# transforms bits to bits adapted for LED strip
 function _transform(message::AbstractArray{UInt8})
-    output = message
+    output = UInt8[]
+
+    for x in message
+        template = 0b100100100100100100100100
+
+        # create 24 bit from 8 bits
+        y = copy(x)
+        for i in 0:7
+            digit = y % 2
+            template = digit << (1 + 3*i) | template
+            y >>= 1
+        end
+        
+        # splitting 24 bits to three UInt8
+        part3 = template % 2^8
+        template >>= 8
+        part2 = template % 2^8
+        template >>= 8
+        part1 = template % 2^8
+
+        push!(output, part1, part2, part3)
+    end
 
     output
 end
